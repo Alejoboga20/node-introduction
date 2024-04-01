d1644f8f750ce5ec65ef5e23ea587a6a4ab78afa
alejoboga20 <alejoboga19@gmail.com>
2024-04-01 14:47:55 -0300

    execution order

---
+++ execution-order.js
@@ -0,0 +1,15 @@
+console.log('init');
+
+setTimeout(() => {
+	console.log('first timeout');
+}, 3000);
+
+setTimeout(() => {
+	console.log('second timeout');
+}, 0);
+
+setTimeout(() => {
+	console.log('third timeout');
+}, 0);
+
+console.log('end');
