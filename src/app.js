3d8d8ef5642239ea1e2e626f807ae9c1e1064a4d
alejoboga20 <alejoboga19@gmail.com>
2024-04-03 19:58:05 -0300

    env variables

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 const { emailTemplate } = require('./js-foundations/01-template');
+require('./js-foundations/02-destructuring');
 
 console.log({ emailTemplate });
---
+++ src/js-foundations/02-destructuring.js
@@ -0,0 +1,8 @@
+// console.log('port: ', process.env.PORT ?? 3000);
+
+const { SHELL, HOMEBREW_PREFIX } = process.env;
+// console.log({ SHELL, HOMEBREW_PREFIX });
+
+const characters = ['Goku', 'Vegeta', 'Trunks'];
+const [goku, vegeta, trunks] = characters;
+// console.log({ goku, vegeta, trunks });
