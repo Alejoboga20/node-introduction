99b69954fc49bd008fca9da1fa25aeeaa1f67bbd
alejoboga20 <alejoboga19@gmail.com>
2024-04-04 15:34:42 -0300

    callbacks

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
-const { emailTemplate } = require('./js-foundations/01-template');
-require('./js-foundations/02-destructuring');
+// const { emailTemplate } = require('./js-foundations/01-template');
+// require('./js-foundations/02-destructuring');
+const { getUserById } = require('./js-foundations/03-callbacks');
 
-console.log({ emailTemplate });
+getUserById(1, (error, user) => {
+	if (error) {
+		throw new Error(error);
+	}
+	console.log(user);
+});
