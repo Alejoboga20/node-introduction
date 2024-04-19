3c7b82da4a73c9a8482d97793844a74665797a63
alejoboga20 <alejoboga19@gmail.com>
2024-04-19 15:57:40 -0300

    arrange act assert

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,11 @@
 describe('App', () => {
 	test('should be true', () => {
-		expect(true).toBe(true);
+		// Arrange
+		const num1 = 1;
+		const num2 = 2;
+		// Act
+		const result = num1 + num2;
+		// Assert
+		expect(result).toBe(3);
 	});
 });
