fbb4536adc4574f5b368fe81437c7144704ae27c
alejoboga20 <alejoboga19@gmail.com>
2024-04-23 15:33:22 -0300

    test success

--- tests/js-foundations/03-callbacks.test.ts
+++ tests/js-foundations/03-callbacks.test.ts
@@ -1,12 +1,24 @@
 import { getUserById } from '../../src/js-foundations/03-callbacks';
 
-const USER_ID = 10;
+const USER_ID = 1;
+const USER_ID_NOT_FOUND = 10;
+
+const userBench = {
+	id: 1,
+	name: 'John',
+};
 
 describe('Callback Tests', () => {
-	test('should throw user not found', () => {
+	test('should return user', () => {
 		getUserById(USER_ID, (error, user) => {
-			console.log({ user, error });
-			expect(error).toBe(`User with id ${USER_ID} not found`);
+			expect(error).toBeUndefined();
+			expect(user).toEqual(userBench);
+		});
+	});
+
+	test('should throw user not found', () => {
+		getUserById(USER_ID_NOT_FOUND, (error, user) => {
+			expect(error).toBe(`User with id ${USER_ID_NOT_FOUND} not found`);
 			expect(user).toBeUndefined();
 		});
 	});
